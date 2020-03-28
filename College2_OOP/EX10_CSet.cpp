fa7f20fa8f54d1b8a9f8c7b2fb7ce7353b324b08
i-rinak <i-rinka@outlook.com>
2020-03-28 14:54:39 +0800

    20.3.28

--- College2_OOP/EX10_CSet.cpp
+++ College2_OOP/EX10_CSet.cpp
@@ -8,9 +8,26 @@ private:
     int *member;
 
 public:
-    CSet();
-    CSet(int);
-    CSet(int *, int);
+    CSet()
+    {
+        this->size = 0;
+        this->member = NULL;
+    }
+    CSet(int number)
+    {
+        this->size = 1;
+        member = new (int);
+        *member = number;
+    }
+    CSet(int *a, int length)
+    {
+        this->member = (int *)malloc(sizeof(int) * length);
+        this->size = length;
+        for (int i = 0; i < length; i++)
+        {
+            this->member[i] = a[i];
+        }
+    }
     CSet operator+(const CSet &other)
     {
         CSet NewSet;
@@ -18,17 +35,36 @@ public:
         NewSet.member = (int *)malloc(sizeof(int) * NewSet.size);
         for (int i = 0; i < this->size; i++)
         {
+            NewSet.member[i] = this->member[i];
+        }
+        for (int i = 0; i < other.size; i++)
+        {
+            NewSet.member[i + this->size] = other.member[i];
+        }
+        return NewSet;
+    }
+    void operator=(const CSet &Set)
+    {
+        free(this->member);
+        this->size = Set.size;
+        this->member = (int *)malloc(sizeof(int) * Set.size);
+        for (int i = 0; i < this->size; i++)
+        {
+            this->member[i] = Set.member[i];
         }
     }
-    ~CSet();
+    ~CSet()
+    {
+        free(this->member);
+    }
     friend ostream &operator<<(ostream &output, const CSet &other)
     {
         output << '[';
         for (int i = 0; i < other.size; i++)
         {
-            output << ' ' << '[' << other.member[i] << ']';
+            output << '[' << other.member[i] << ']';
         }
-        output << ' ' << ']';
+        output << ']' << endl;
         return output;
     }
 };
